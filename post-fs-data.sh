454b9dd7a2b1fcc4ba4498f1854152742e456ba4
Tatsh Siow <blussit@gmail.com>
2024-11-26 16:26:35 +0800

    v2.0

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -4,3 +4,192 @@
 # This script will be executed in post-fs-data mode
 # More info in the main Magisk thread
 MODDIR=${0%/*}
+
+####################################
+# Disable Unnecessary Things (by @nonosvaimos)
+####################################
+resetprop -n av.debug.disable.pers.cache true
+resetprop -n config.disable_rtt true
+resetprop -n config.stats 0
+resetprop -n db.log.slow_query_threshold 0
+resetprop -n debug.atrace.tags.enableflags false
+resetprop -n debug.egl.profiler 0
+resetprop -n debug.enable.gamed false
+resetprop -n debug.enable.wl_log false
+resetprop -n debug.hwc.otf 0
+resetprop -n debug.hwc_dump_en 0
+resetprop -n debug.mdpcomp.logs 0
+resetprop -n debug.qualcomm.sns.daemon 0
+resetprop -n debug.qualcomm.sns.libsensor1 0
+resetprop -n debug.sf.ddms 0
+resetprop -n debug.sf.disable_client_composition_cache 1
+resetprop -n debug.sf.dump 0
+resetprop -n debug.sqlite.journalmode OFF
+resetprop -n debug.sqlite.syncmode=OFF
+resetprop -n debug.sqlite.wal.syncmode OFF
+resetprop -n debug_test 0
+resetprop -n libc.debug.malloc 0
+resetprop -n log.shaders 0
+resetprop -n log.tag.all 0
+resetprop -n log.tag.stats_log OFF
+resetprop -n log_ao 0
+resetprop -n log_frame_info 0
+resetprop -n logd.logpersistd.enable false
+resetprop -n logd.statistics 0
+resetprop -n media.metrics.enabled false
+resetprop -n media.metrics 0
+resetprop -n media.stagefright.log-uri 0
+resetprop -n net.ipv4.tcp_no_metrics_save 1
+resetprop -n persist.anr.dumpthr 0
+resetprop -n persist.data.qmi.adb_logmask 0
+resetprop -n persist.debug.sensors.hal 0 
+resetprop -n persist.debug.wfd.enable false
+resetprop -n persist.ims.disableADBLogs true
+resetprop -n persist.ims.disabled true
+resetprop -n persist.ims.disableDebugLogs true
+resetprop -n persist.ims.disableIMSLogs true
+resetprop -n persist.ims.disableQXDMLogs true
+resetprop -n persist.logd.limit OFF
+resetprop -n persist.logd.size OFF
+resetprop -n persist.logd.size.crash OFF
+resetprop -n persist.logd.size.main OFF
+resetprop -n persist.logd.size.radio OFF
+resetprop -n persist.logd.size.system OFF
+resetprop -n persist.logd.size OFF
+resetprop -n persist.oem.dump 0
+resetprop -n persist.service.logd.enable false
+resetprop -n persist.sys.perf.debug false
+resetprop -n persist.sys.ssr.enable_debug false
+resetprop -n persist.sys.ssr.restart_level 1
+resetprop -n persist.sys.strictmode.disable true
+resetprop -n persist.traced.enable false
+resetprop -n persist.traced_perf.enable false
+resetprop -n persist.vendor.crash.cnt 0
+resetprop -n persist.vendor.crash.detect false
+resetprop -n persist.vendor.radio.adb_log_on 0
+resetprop -n persist.vendor.radio.snapshot_enabled false
+resetprop -n persist.vendor.radio.snapshot_timer 0
+resetprop -n persist.vendor.sys.modem.logging.enable false
+resetprop -n persist.vendor.sys.reduce_qdss_log 1
+resetprop -n persist.vendor.verbose_logging_enabled false
+resetprop -n persist.wpa_supplicant.debug false
+resetprop -n ro.config.ksm.support false
+resetprop -n ro.config.nocheckin 1
+resetprop -n ro.debuggable 0
+resetprop -n ro.kernel.android.checkjni 0
+resetprop -n ro.kernel.checkjni 0
+resetprop -n ro.logd.kernel false
+resetprop -n ro.logd.size.stats OFF
+resetprop -n ro.logd.size OFF
+resetprop -n ro.logdumpd.enabled false
+resetprop -n ro.statsd.enable false
+resetprop -n ro.telephony.call_ring.multiple false
+resetprop -n ro.vendor.connsys.dedicated.log 0
+resetprop -n rw.logger 0
+resetprop -n sys.miui.ndcd 0
+resetprop -n sys.wifitracing.started 0
+resetprop -n vendor.vidc.debug.level 0
+resetprop -n vidc.debug.level 0
+
+####################################
+# LMK (by @nonosvaimos)
+####################################
+resetprop -n ro.lmk.debug false
+resetprop -n ro.lmk.log_stats false
+
+####################################
+# Dalvik (by @nonosvaimos)
+####################################
+resetprop -n dalvik.gc.type precise
+resetprop -n dalvik.vm.check-dex-sum false
+resetprop -n dalvik.vm.checkjni false
+resetprop -n dalvik.vm.dex2oat-minidebuginfo false
+resetprop -n dalvik.vm.minidebuginfo false
+resetprop -n dalvik.vm.verify-bytecode false
+
+####################################
+# DalvikHyperthreading (by @modulostk)
+####################################
+resetprop -n persist.sys.dalvik.hyperthreading true
+resetprop -n persist.sys.dalvik.multithread true
+
+####################################
+# Optimizing Texture for Performance
+####################################
+resetprop -n ro.hwui.texture_cache_size 72
+resetprop -n ro.hwui.layer_cache_size 48
+resetprop -n ro.hwui.r_buffer_cache_size 8
+resetprop -n ro.hwui.path_cache_size 32
+resetprop -n ro.hwui.gradient_cache_size 1
+resetprop -n ro.hwui.drop_shadow_cache_size 6
+resetprop -n ro.hwui.texture_cache_flushrate 0.4
+resetprop -n ro.hwui.text_small_cache_width 1024
+resetprop -n ro.hwui.text_small_cache_height 1024
+resetprop -n ro.hwui.text_large_cache_width 2048
+resetprop -n ro.hwui.text_large_cache_height 2048
+
+####################################
+# Tombstone (by @modulostk)
+####################################
+# Max tombstone count [/data/tombstones]
+resetprop -n tombstoned.max_tombstone_count 0
+# Max anr tombstone count [/data/anr]
+resetprop -n tombstoned.max_anr_count 0
+
+#dropbox disabler
+settings put global dropbox:dumpsys:procstats disabled
+settings put global dropbox:dumpsys:usagestats disabled
+
+#@@@@@@ Custom Rulesets @@@@@@@@@@@
+#Optimize Surface Flinger Latch
+resetprop -n debug.sf.latch_unsignaled false
+resetprop -n debug.sf.auto_latch_unsignaled false
+
+# IORAPD can improve app launch speeds
+resetprop -n ro.iorapd.enable true
+
+# Android ART Logd
+resetprop -n persist.sys.qlogd 0
+
+# Disable logging and debugging stuff
+resetprop -n debugtool.anrhistory 0
+resetprop -n debug.sf.enable_egl_image_tracker 0
+resetprop -n persist.debug.sf.statistics 0
+resetprop -n persist.log.tag.snet_event_log OFF
+resetprop -n persist.radio.ramdump 0
+resetprop -n persist.sys.lmk.reportkills false
+resetprop -n persist.sys.offlinelog.kernel false
+resetprop -n persist.sys.offlinelog.logcat false
+resetprop -n persist.sys.offlinelog.logcatkernel false
+resetprop -n persist.sys.offlinelog.bootlog false
+resetprop -n persist.sys.whetstone.level 0
+resetprop -n profiler.hung.dumpdobugreport false
+resetprop -n profiler.launch false
+resetprop -n logd.disable 1
+resetprop -n persist.sys.qseelogd false
+resetprop -n ro.boot.ramdump disable
+resetprop -n persist.oem.dump 0
+resetprop -n persist.vendor.ssr.enable_ramdumps 0
+resetprop -n vendor.bluetooth.startbtlogger false
+resetprop -n vendor.swvdec.log.level 0
+# Dynamic sampling rates for certain UI rendering operations 
+resetprop -n dev.pm.dyn_samplingrate 1
+# Precompiles app layouts(reducing runtime CPU usage for UI tasks)
+resetprop -n dev.pm.precompile_layouts 1
+# System Performance Thread Optimization(Improve task scheduling efficiency)
+resetprop -n persist.sys.miui_sptm.enable true
+#Performance Tuning
+resetprop -n accelerated_enabled_for_all true
+resetprop -n ro.hwui.disable_scissor_opt false
+resetprop -n ro.zygote.disable_gl_preload false
+resetprop -n debug.sf.enable_hwc_vds 1
+
+# Zero Shutter Lag
+resetprop -n camera.disable_zsl_mode 0
+
+#Testing
+resetprop -n debug.power.monitor_tools false
+resetprop -n debug.sf.disable_backpressure 1
+resetprop -n debug.sf.enable_gl_backpressure 1
+resetprop -n persist.mm.enable.prefetch false
+resetprop -n vendor.gralloc.disable_ubwc false
--- service.sh
+++ service.sh
@@ -4,18 +4,147 @@
 # This script will be executed in late_start service mode
 # More info in the main Magisk thread
 
-#Enable MGLRU (Improve App Launch Time, much power efficient)
-#After testing, it uses more power to maintain background process
-#if [ -f /sys/kernel/mm/lru_gen/enabled ]; then
-#  echo "0x0003" > /sys/kernel/mm/lru_gen/enabled
-#  chmod 444 /sys/kernel/mm/lru_gen/enabled
-#fi
-#
-#if [ -f /sys/kernel/mm/lru_gen/min_ttl_ms ]; then
-#  echo "1000" > /sys/kernel/mm/lru_gen/min_ttl_ms
-#  chmod 444 /sys/kernel/mm/lru_gen/min_ttl_ms
-#fi
-#
+wait_until_login() {
+  while [[ "$(getprop sys.boot_completed)" != "1" ]]; do
+    sleep 3
+  done
+
+  test_file="/storage/emulated/0/Android/.PERMISSION_TEST"
+  true >"$test_file"
+  while [[ ! -f "$test_file" ]]; do
+    true >"$test_file"
+    sleep 1
+  done
+  rm -f "$test_file"
+}
+
+wait_until_login
+sleep 60
+
+####################################
+# Tweaking Android (thx to Melody Script https://github.com/ionuttbara/melody_android)
+####################################
+su -c "cmd settings put global activity_starts_logging_enabled 0"
+su -c "cmd settings put global ble_scan_always_enabled 0"
+su -c "cmd settings put global hotword_detection_enabled 0"
+su -c "cmd settings put global mobile_data_always_on 0"
+su -c "cmd settings put global network_recommendations_enabled 0"
+su -c "cmd settings put global wifi_scan_always_enabled 0"
+su -c "cmd settings put secure adaptive_sleep 0"
+su -c "cmd settings put secure screensaver_activate_on_dock 0"
+su -c "cmd settings put secure screensaver_activate_on_sleep 0" 
+su -c "cmd settings put secure screensaver_enabled 0"
+su -c "cmd settings put secure send_action_app_error 0"
+su -c "cmd settings put system air_motion_engine 0"
+su -c "cmd settings put system air_motion_wake_up 0"
+su -c "cmd settings put system intelligent_sleep_mode 0"
+su -c "cmd settings put system master_motion 0"
+su -c "cmd settings put system motion_engine 0"
+su -c "cmd settings put system nearby_scanning_enabled 0"
+su -c "cmd settings put system nearby_scanning_permission_allowed 0"
+#this is start of custom line
+su -c "cmd settings put system send_security_reports 0"
+su -c "cmd settings put global debug.gralloc.enable_fb_ubwc 1"
+su -c "cmd settings put global debug.sf.hw 1"
+su -c "cmd settings put global enable_gpu_debug_layers 0"
+su -c "cmd settings put global ecg_disable_logging 1"
+su -c "cmd settings put system call_log 0"
+su -c "cmd settings put global debug.sf.enable_gl_backpressure 1"
+su -c "cmd settings put global personalized_ad_enabled 0"
+su -c "cmd settings put global passport_ad_status OFF"
+su -c "cmd settings put global pmiui_ambient_scan_support 1"
+su -c "cmd settings put global netstats_enabled 0" #disable this if you want to track network stats
+su -c "cmd settings put global fast_connect_ble_scan_mode 0"
+su -c "cmd settings put global TV_SENSOR_JAR_ENABLE 0"
+su -c "cmd settings put global cached_apps_freezer enabled" #Cache AppFreeze
+su -c "cmd settings put system intelligent_sleep_mode 1"
+####################################
+# Services
+####################################
+su -c "stop logcat logcatd logd tcpdump cnss_diag statsd traced idd-logreader idd-logreadermain stats dumpstate aplogd tcpdump vendor.tcpdump vendor_tcpdump vendor.cnss_diag"
+
+####################################
+# Wi-Fi Logs (thx to @LeanHijosdesusMadres)
+####################################
+rm -rf /data/vendor/wlan_logs
+touch /data/vendor/wlan_logs
+chmod 000 /data/vendor/wlan_logs
+
+####################################
+# Useless Services 
+####################################
+su -c "pm disable com.google.android.gms/.chimera.GmsIntentOperationService"
+su -c "pm disable com.google.android.gms/com.google.android.gms.mdm.receivers.MdmDeviceAdminReceiver"
+
+####################################
+# Kernel Debugging (thx to KTSR)
+####################################
+for i in "debug_mask" "log_level*" "debug_level*" "*debug_mode" "enable_ramdumps" "enable_mini_ramdumps" "edac_mc_log*" "enable_event_log" "*log_level*" "*log_ue*" "*log_ce*" "log_ecn_error" "snapshot_crashdumper" "seclog*" "compat-log" "*log_enabled" "tracing_on" "mballoc_debug"; do
+    for o in $(find /sys/ -type f -name "$i"); do
+        echo "0" > "$o"
+    done
+done
+
+for sys in /sys; do
+    echo "1" > "$sys/module/spurious/parameters/noirqdebug"
+    echo "0" > "$sys/kernel/debug/sde_rotator0/evtlog/enable"
+  echo "0" > "$sys/kernel/debug/dri/0/debug/enable"
+  echo "0" > "$sys/module/rmnet_data/parameters/rmnet_data_log_level"
+done
+
+echo "0" > "/proc/sys/debug/exception-trace"
+echo "0" > "/proc/sys/kernel/sched_schedstats"
+  
+####################################
+# CRC
+####################################
+for parameters in /sys/module/mmc_core/parameters/*; do
+    echo "0" > "$parameters/crc"
+    echo "0" > "$parameters/removable"
+    echo "0" > "$parameters/use_spi_crc"
+done
+
+####################################
+# Printk (thx to KNTD-reborn)
+####################################
+echo "0 0 0 0" > "/proc/sys/kernel/printk"
+echo "0" > "/sys/kernel/printk_mode/printk_mode"
+echo "0" > "/sys/module/printk/parameters/cpu"
+echo "0" > "/sys/module/printk/parameters/pid"
+echo "0" > "/sys/module/printk/parameters/printk_ratelimit"
+echo "0" > "/sys/module/printk/parameters/time"
+echo "1" > "/sys/module/printk/parameters/console_suspend"
+echo "1" > "/sys/module/printk/parameters/ignore_loglevel"
+echo "off" > "/proc/sys/kernel/printk_devkmsg"
+
+####################################
+# Ramdumps
+####################################
+for parameters in /sys/module/subsystem_restart/parameters; do
+    echo "0" > "$parameters/enable_mini_ramdumps"
+    echo "0" > "$parameters/enable_ramdumps"
+done
+
+####################################
+# File System
+####################################
+for fs in /proc/sys/fs; do
+    echo "0" > "$fs/by-name/userdata/iostat_enable"
+    echo "0" > "$fs/dir-notify-enable"
+done
+
+####################################
+# Disable Kernel Panic
+####################################
+echo "0" > /proc/sys/kernel/panic
+echo "0" > /proc/sys/kernel/panic_on_oops
+echo "0" > /proc/sys/kernel/panic_on_rcu_stall
+echo "0" > /proc/sys/kernel/panic_on_warn
+echo "0" > /sys/module/kernel/parameters/panic
+echo "0" > /sys/module/kernel/parameters/panic_on_warn
+echo "0" > /sys/module/kernel/parameters/panic_on_oops
+echo "0" > /sys/vm/panic_on_oom
+
 # Disable debug process
 if [ -f /sys/module/binder/parameters/debug_mask ]; then
   echo "0" > /sys/module/binder/parameters/debug_mask
@@ -45,25 +174,12 @@ if [ -f /proc/sys/kernel/printk_devkmsg ]; then
   echo "off" > /proc/sys/kernel/printk_devkmsg
 fi
 
-if [ -f /proc/sys/kernel/sched_schedstats ]; then
-  echo "0" > /proc/sys/kernel/sched_schedstats
-fi
-
 if [ -f /sys/fs/f2fs/sda32/iostat_enable ]; then
   echo "0" > /sys/fs/f2fs/sda32/iostat_enable
 fi
 
 if [ -f /sys/module/millet_core/parameters/millet_debug ]; then
   echo "0" > /sys/module/millet_core/parameters/millet_debug
-fi
-
-#Disable RAMDUMP
-if [ -f sys/module/subsystem_restart/parameters/enable_ramdumps ]; then
-  echo "0" > sys/module/subsystem_restart/parameters/enable_ramdumps
-fi
-
-if [ -f /sys/module/subsystem_restart/parameters/enable_mini_ramdumps ]; then
-  echo "0" > /sys/module/subsystem_restart/parameters/enable_mini_ramdumps
 fi
 
 #Kernel Tuning
@@ -102,15 +218,11 @@ echo 0 > /sys/block/*/queue/nomerges
 
 sleep 5
 
-#kill bootlogoupdater
 am kill bootlogoupdater
 killall -9 bootlogoupdater
 am kill bootlogoupdater.rc
 killall -9 bootlogoupdater.rc
 
-sleep 5
-
-#kill useless logging service
 am kill mdnsd
 killall -9 mdnsd
 am kill mdnsd.rc
