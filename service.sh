fb021957d65664d76dc4e3f72a1c11e14740c329
Tatsh Siow <blussit@gmail.com>
2024-11-28 16:47:02 +0800

    v2.1

--- service.sh
+++ service.sh
@@ -1,9 +1,3 @@
-#!/system/bin/sh
-# Please don't hardcode /magisk/modname/... ; instead, please use $MODDIR/...
-# This will make your scripts compatible even if Magisk change its mount point in the future
-# This script will be executed in late_start service mode
-# More info in the main Magisk thread
-
 wait_until_login() {
   while [[ "$(getprop sys.boot_completed)" != "1" ]]; do
     sleep 3
@@ -17,18 +11,35 @@ wait_until_login() {
   done
   rm -f "$test_file"
 }
+####################################
+# release caches when boot
+####################################
+echo 3 > /proc/sys/vm/drop_caches
+echo 1 > /proc/sys/vm/compact_memory
 
 wait_until_login
 sleep 60
 
 ####################################
-# Tweaking Android (thx to Melody Script https://github.com/ionuttbara/melody_android)
+# Tweaking Android (Github:ionuttbara + Tatsh)
 ####################################
 su -c "cmd settings put global activity_starts_logging_enabled 0"
 su -c "cmd settings put global ble_scan_always_enabled 0"
+su -c "cmd settings put global cached_apps_freezer enabled" #Cache AppFreeze
+su -c "cmd settings put global debug.gralloc.enable_fb_ubwc 1"
+su -c "cmd settings put global debug.sf.enable_gl_backpressure 1"
+su -c "cmd settings put global debug.sf.hw 1"
+su -c "cmd settings put global enable_gpu_debug_layers 0"
+su -c "cmd settings put global ecg_disable_logging 1"
+su -c "cmd settings put global fast_connect_ble_scan_mode 0"
 su -c "cmd settings put global hotword_detection_enabled 0"
 su -c "cmd settings put global mobile_data_always_on 0"
+su -c "cmd settings put global netstats_enabled 0" #disable this if you want to track data network stats
 su -c "cmd settings put global network_recommendations_enabled 0"
+su -c "cmd settings put global passport_ad_status OFF"
+su -c "cmd settings put global personalized_ad_enabled 0"
+su -c "cmd settings put global pmiui_ambient_scan_support 0"
+su -c "cmd settings put global TV_SENSOR_JAR_ENABLE 0"
 su -c "cmd settings put global wifi_scan_always_enabled 0"
 su -c "cmd settings put secure adaptive_sleep 0"
 su -c "cmd settings put secure screensaver_activate_on_dock 0"
@@ -37,38 +48,13 @@ su -c "cmd settings put secure screensaver_enabled 0"
 su -c "cmd settings put secure send_action_app_error 0"
 su -c "cmd settings put system air_motion_engine 0"
 su -c "cmd settings put system air_motion_wake_up 0"
+su -c "cmd settings put system call_log 0" #won't affect miui caller
 su -c "cmd settings put system intelligent_sleep_mode 0"
 su -c "cmd settings put system master_motion 0"
 su -c "cmd settings put system motion_engine 0"
 su -c "cmd settings put system nearby_scanning_enabled 0"
 su -c "cmd settings put system nearby_scanning_permission_allowed 0"
-#this is start of custom line
 su -c "cmd settings put system send_security_reports 0"
-su -c "cmd settings put global debug.gralloc.enable_fb_ubwc 1"
-su -c "cmd settings put global debug.sf.hw 1"
-su -c "cmd settings put global enable_gpu_debug_layers 0"
-su -c "cmd settings put global ecg_disable_logging 1"
-su -c "cmd settings put system call_log 0"
-su -c "cmd settings put global debug.sf.enable_gl_backpressure 1"
-su -c "cmd settings put global personalized_ad_enabled 0"
-su -c "cmd settings put global passport_ad_status OFF"
-su -c "cmd settings put global pmiui_ambient_scan_support 1"
-su -c "cmd settings put global netstats_enabled 0" #disable this if you want to track network stats
-su -c "cmd settings put global fast_connect_ble_scan_mode 0"
-su -c "cmd settings put global TV_SENSOR_JAR_ENABLE 0"
-su -c "cmd settings put global cached_apps_freezer enabled" #Cache AppFreeze
-su -c "cmd settings put system intelligent_sleep_mode 1"
-####################################
-# Services
-####################################
-su -c "stop logcat logcatd logd tcpdump cnss_diag statsd traced idd-logreader idd-logreadermain stats dumpstate aplogd tcpdump vendor.tcpdump vendor_tcpdump vendor.cnss_diag"
-
-####################################
-# Wi-Fi Logs (thx to @LeanHijosdesusMadres)
-####################################
-rm -rf /data/vendor/wlan_logs
-touch /data/vendor/wlan_logs
-chmod 000 /data/vendor/wlan_logs
 
 ####################################
 # Useless Services 
@@ -86,8 +72,8 @@ for i in "debug_mask" "log_level*" "debug_level*" "*debug_mode" "enable_ramdumps
 done
 
 for sys in /sys; do
-    echo "1" > "$sys/module/spurious/parameters/noirqdebug"
-    echo "0" > "$sys/kernel/debug/sde_rotator0/evtlog/enable"
+  echo "1" > "$sys/module/spurious/parameters/noirqdebug"
+  echo "0" > "$sys/kernel/debug/sde_rotator0/evtlog/enable"
   echo "0" > "$sys/kernel/debug/dri/0/debug/enable"
   echo "0" > "$sys/module/rmnet_data/parameters/rmnet_data_log_level"
 done
@@ -214,56 +200,53 @@ echo 0 > /sys/block/*/queue/iostats
 #Disable All I/O Debug Helper
 echo 0 > /sys/block/*/queue/nomerges
 
-###The script Above is moved from post-fs-data.sh
+###########################Divider###########################
 
 sleep 5
 
-am kill bootlogoupdater
-killall -9 bootlogoupdater
-am kill bootlogoupdater.rc
-killall -9 bootlogoupdater.rc
+process="
+aee.log-1-0
+aee.log-1-0.rc
+aplogd
+bootlogoupdater
+bootlogoupdater.rc
+cnss_diag
+connsyslogger
+connsyslogger.rc
+dumpstate
+dumpstate.rc
+emdlogger1
+emdlogger1.rc
+emdlogger3
+emdlogger3.rc
+idd-logreader
+idd-logreadermain
+logcat
+logcatd
+logd
+logd.rc
+mdlogger
+mdlogger.rc
+mdnsd
+mdnsd.rc
+mobile_log_d
+mobile_log_d.rc
+stats
+statsd
+tcpdump
+traced
+vendor.tcpdump
+vendor_tcpdump
+vendor.cnss_diag"
 
-am kill mdnsd
-killall -9 mdnsd
-am kill mdnsd.rc
-killall -9 mdnsd.rc
+for name in $process; do
+  am kill "$name"
+  killall -9 "$name"
+done
 
-am kill mobile_log_d
-killall -9 mobile_log_d
-am kill mobile_log_d.rc
-killall -9 mobile_log_d.rc
-
-am kill dumpstate
-killall -9 dumpstate
-am kill dumpstate.rc
-killall -9 dumpstate.rc
-
-am kill emdlogger1
-killall -9 emdlogger1
-am kill emdlogger1.rc
-killall -9 emdlogger1.rc
-
-am kill emdlogger3
-killall -9 emdlogger3
-am kill emdlogger3.rc
-killall -9 emdlogger3.rc
-
-am kill mdlogger
-killall -9 mdlogger
-am kill mdlogger.rc
-killall -9 mdlogger.rc
-
-am kill aee.log-1-0
-killall -9 aee.log-1-0
-am kill aee.log-1-0.rc
-killall -9 aee.log-1-0.rc
-
-am kill connsyslogger
-killall -9 connsyslogger
-am kill connsyslogger.rc
-killall -9 connsyslogger.rc
-
-am kill bootlogoupdater
-killall -9 bootlogoupdater
-am kill bootlogoupdater.rc
-killall -9 bootlogoupdater.rc
+####################################
+# Wi-Fi Logs
+####################################
+rm -rf /data/vendor/wlan_logs
+touch /data/vendor/wlan_logs
+chmod 000 /data/vendor/wlan_logs
